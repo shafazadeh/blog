ae57ed701248a1b66d4f3a5579c6e7eea038ba4e
narges shafazadeh <n.shafazadeh@gmail.com>
2025-04-13 22:48:40 +0330

    add fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme")
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,mjs}"],
-  darkMode: "class", // allows toggling dark mode manually
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Roboto", "sans-serif", ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
-  plugins: [require("@tailwindcss/typography")],
-}
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,mjs}'],
+	darkMode: 'class', // allows toggling dark mode manually
+	theme: {
+		extend: {
+			fontFamily: {
+				// sans: ['Roboto', 'sans-serif', ...defaultTheme.fontFamily.sans],
+				body: ['Light', 'Black', 'Bold', 'Medium', 'UltraLight'],
+			},
+		},
+	},
+	plugins: [require('@tailwindcss/typography')],
+};
